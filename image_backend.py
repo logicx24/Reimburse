c7861796cb799bd0a9392c6cc2e6c59cdd2057e5
Aakash Japi <aakashjapi@gmail.com>
2015-05-21 19:33:16 -0400

    route set

--- image_backend.py
+++ image_backend.py
@@ -5,9 +5,17 @@ import numpy as np
 from matplotlib import pyplot as plt
 from StringIO import StringIO
 #from SimpleCV import *
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import requests
+import json,httplib,urllib
+
+r = 0
 
 app = Flask(__name__)
+
+appID = "M5W9yL6PGhFQwd1bjtLg9Uaq8LiwrCNuDULXrLpA"
+      
+restKey = "D6KZxe2dRp1Wa8Drfpjer0iNOHNZATRkDaXXCR3Z"
 
 def image_to_text(filename):
 	#return pytesseract.image_to_string(Image.open('receipts/{0}'.format(filename)))
@@ -37,6 +45,7 @@ def text_parsing(extractedText):
 	extracted = extractedText.split("\n")
 	subdex = 0
 	taxdex = 0
+	print(extracted)
 	for i in range(len(extracted)):
 		if "ubtot" in extracted[i]:
 			subdex = i
@@ -44,12 +53,35 @@ def text_parsing(extractedText):
 			taxdex = i
 	return float(extracted[subdex].split()[-1]) + float(extracted[taxdex].split()[-1])
 
-@app.route('/images', methods=['POST'])
+@app.route('/images/', methods=['POST'])
 def image_route():
-	
+	global r
+	payload = {"X-Parse-Application-Id": appID, "X-Parse-REST-API-Key": restKey}
+	headers = {'where' : {'Processed':False}}
+	#imageJSON = requests.get('http://api.parse.com/1/classes/Transaction/', params=payload, headers=headers)
+
+	connection = httplib.HTTPSConnection('api.parse.com', 443)
+	params = urllib.urlencode({"where":json.dumps({
+       "Processed": False
+     })})
+	connection.connect()
+	connection.request('GET', '/1/classes/Transaction?%s' % params, '', {
+       "X-Parse-Application-Id": appID,
+       "X-Parse-REST-API-Key": restKey
+     })
+	result = json.loads(connection.getresponse().read())
+	#image = imageJSON.body['images'][listIndex]
+	#open('receipts/s{0}.jpg'.format(str(r)), 'w').write(lr)
+	open('receipts/{0}.jpg'.format('s' + str(r)),'w').write(urllib.urlopen(result['results'][0]['Receipt']['url']).read())
+	price = text_parsing(image_to_text('s{0}.jpg'.format(str(r))))
+	r += 1
+	return json.dumps({'price':price})
 
 
+if __name__ == "__main__":
+	app.run()
 
+#"X-Parse-Application-Id: M5W9yL6PGhFQwd1bjtLg9Uaq8LiwrCNuDULXrLpA" -H "X-Parse-REST-API-Key: D6KZxe2dRp1Wa8Drfpjer0iNOHNZATRkDaXXCR3Z"
 
 
 
