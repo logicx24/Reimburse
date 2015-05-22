c50c9b439390d355680ca572ee8fe368ec8c10ca
Mosab Elagha <mosabelagha@gmail.com>
2015-05-22 09:00:59 -0400

    works k

--- website/client/app/main/main.controller.js
+++ website/client/app/main/main.controller.js
@@ -1,29 +1,29 @@
 'use strict';
-
+ 
 angular.module('websiteApp')
   .controller('MainCtrl', function ($scope, $http, pass) {
     $scope.awesomeThings = [];
-    
-//    $scope.allTrips = 'hiiii';
-
+ 
+    $scope.allTrips = 'hiiii';
+ 
     $scope.array = [{id:1, value:"test"}, {id:2, value:"test1"}];
-        
-    $scope.$on('$viewContentLoaded', function() {      
+ 
+    $scope.$on('$viewContentLoaded', function() {
         $scope.parseInit();
         $scope.getTripData();
     });
-
+ 
     $scope.clicked = function(a) {
       pass.myData = a;
       console.log('yo' + a.id);
     };
-
+ 
     $scope.parseInit = function() {
       var applicationId = "M5W9yL6PGhFQwd1bjtLg9Uaq8LiwrCNuDULXrLpA";
       var javaScriptKey = "CfiSYmOt0TLu8k9xzJObKA6GosGwbIdv5IiyfI4F";
       Parse.initialize(applicationId, javaScriptKey);
     }
-
+ 
     // get list of all trips
     $scope.getTripData = function(){
       var query = new Parse.Query("Trips");
@@ -31,32 +31,38 @@ angular.module('websiteApp')
         success: function (trips) {
           //Trips
           $scope.allTrips = trips;
-            console.log($scope.allTrips);
+          console.log($scope.allTrips);
+          $scope.$apply();
         },
         error: function (error) {
-             console.log(error);
+          console.log(error);
         }
       });
     }
-  $scope.helloWorld = "hello world"
-  $scope.goodbyeWorld = "goodbye"
-//OnClick of Trip function
-//    $scope.trans = function (objectId) {
-//        var json = [];
-//        console.log(objectId);
-//        var query = new Parse.Query("Transaction");
-//        query.containedIn("Trip", [objectId]);
-//        query.find({
-//          success: function (transactions) {
-//              $scope.transactions = transactions;
-//              for (var j = 0; j < transactions.length; j++) {
-//                  json.push(transactions[j]);
-//              }
-//          }
-//        });
-//        console.log(json);
-//        return json;
-//    }
-      
+ 
+     // OnClick of Trip function
+     $scope.getTransaction = function (tripId) {
+         var query = new Parse.Query("Transaction");
+         query.containedIn("Trip", [tripId]);
+         query.find({
+           success: function (transactions) {
+               $scope.transactions = transactions;
+               console.log($scope.transactions);
+               $scope.$apply();
+           },
+           error: function (error){
+             console.log(error);
+           }
+         });
+     }
+ 
+     $scope.setCurrentReceipt = function (trans) {
+      console.log(trans);
+        $scope.receipt = trans.attributes.Receipt;
+        $scope.receiptInfo = trans.attributes;
+        console.log($scope.receipt);
+        // $scope.$apply();
+     }
+ 
 //Main end
 });
